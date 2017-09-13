afba7a4898427d8eaf00aadd990d7d7e6c461c3f
Nathanael Jones <nathanael.jones@gmail.com>
2017-09-12 23:12:56 -0600

    Add PinBox.cs

---
+++ src/Imageflow/Bindings/PinBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Imageflow.Net.Bindings
+{
+    internal class PinBox : CriticalFinalizerObject, IDisposable
+    {
+        private List<GCHandle> _pinned;
+        internal void AddPinnedData(GCHandle handle)
+        {
+            if (_pinned == null) _pinned = new List<GCHandle>();
+            _pinned.Add(handle);
+        }
+
+        public void Dispose()
+        {
+            UnpinAll();
+            GC.SuppressFinalize(this);
+        }
+
+        private void UnpinAll()
+        {
+            //Unpin GCHandles
+            if (_pinned != null)
+            {
+                foreach (var active in _pinned)
+                {
+                    if (active.IsAllocated) active.Free();
+                }
+                _pinned = null;
+            }
+        }
+
+        ~PinBox()
+        {
+            UnpinAll();
+        }
+    }
+}
