81ea2638bdffffeaedc0ac9eb86e0ab34e03b0b6
Lilith River <lilith@imazen.io>
2024-01-28 17:20:50 -0700

    Add AOT testing

--- tests/Imageflow.TestWebAOT/Program.cs
+++ tests/Imageflow.TestWebAOT/Program.cs
@@ -12,30 +12,41 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 var app = builder.Build();
 
+app.MapGet("/", () => Results.Text("Hello, world!"));
+
 var imageflowApi = app.MapGroup("/imageflow");
 imageflowApi.MapGet("/version", () =>
 {
     using var c = new JobContext();
     var vi = c.GetVersionInfo();
-    
     return Results.Ok(vi);
 });
 imageflowApi.MapGet("/resize/width/{width}", async(int width) =>
-{
-    var imgBytes = Helpers.GetResourceBytes("icon.png");
-    var job = await new Imageflow.Fluent.ImageJob()
-        .Decode(imgBytes)
-        .Constrain(new Constraint((uint)width, 0))
-        .EncodeToBytes(new Imageflow.Fluent.MozJpegEncoder(90)).Finish().InProcessAsync();
-    var resultBytes = job.First!.TryGetBytes()!.Value;
-    var resultMemory = new Memory<byte>(resultBytes.Array, resultBytes.Offset, resultBytes.Count);
+{  
+    var resultMemory = await Helpers.SizeIcon(width);
     return Results.Bytes(resultMemory, "image/jpeg");
 });
+
+// Test basics during startup
+using var c = new JobContext();
+var vi = c.GetVersionInfo();
+var t = Helpers.SizeIcon(10);
+var resultMemory = t.Result;
 
 app.Run();
 
 internal static class Helpers
 {
+    public static async Task<Memory<byte>> SizeIcon(int width)
+    {
+        var imgBytes = Helpers.GetResourceBytes("icon.png");
+        var job = await new Imageflow.Fluent.ImageJob()
+            .Decode(imgBytes)
+            .Constrain(new Constraint((uint)width, 0))
+            .EncodeToBytes(new Imageflow.Fluent.MozJpegEncoder(90)).Finish().InProcessAsync();
+        var resultBytes = job.First!.TryGetBytes()!.Value;
+        return new Memory<byte>(resultBytes.Array, resultBytes.Offset, resultBytes.Count);
+    }
     public static byte[] GetResourceBytes(string resourceName)
     {
         var assembly = Assembly.GetExecutingAssembly();
